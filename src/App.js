713462ca3180653c0c4bb095fe079e70a15ae52e
jaytrepka <jaychess17@gmail.com>
2017-09-07 14:09:51 +0200

    Tip component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import LetterPicker from './LetterPicker';
+import React, { Component } from "react";
+import "./App.css";
+import LetterPicker from "./LetterPicker";
+import Tip from "./Tip";
 
 class App extends Component {
   state = {
-    password: '',
-    username: '',
-    letter: '',
+    password: "",
+    username: "",
+    letter: "",
     goLeft: false,
     speed: 500,
-    selectedSentence: 0,
+    selectedSentence: 0
   };
 
   handleLetterChange = letter => {
@@ -20,70 +21,70 @@ class App extends Component {
     const { key, target: { name } } = event;
     console.log(key);
 
-    if (key !== 'Tab' && key !== 'Enter') {
+    if (key !== "Tab" && key !== "Enter") {
       event.preventDefault();
     }
 
-    if (key === 'ArrowLeft') {
+    if (key === "ArrowLeft") {
       this.setState({ goLeft: true });
       return;
     }
 
-    if (key === 'ArrowRight') {
+    if (key === "ArrowRight") {
       this.setState({ goLeft: false });
       return;
     }
 
-    if (key === 'ArrowUp') {
+    if (key === "ArrowUp") {
       const newSpeed = Math.max(100, this.state.speed - 100);
       this.setState({ speed: newSpeed });
       return;
     }
 
-    if (key === 'ArrowDown') {
+    if (key === "ArrowDown") {
       const newSpeed = Math.min(800, this.state.speed + 100);
       this.setState({ speed: newSpeed });
       return;
     }
 
-    if ([...'qwert'].indexOf(key) > -1) {
+    if ([..."qwert"].indexOf(key) > -1) {
       this.setState({ selectedSentence: 0 });
       return;
     }
 
-    if ([...'yuiop'].indexOf(key) > -1) {
+    if ([..."yuiop"].indexOf(key) > -1) {
       this.setState({ selectedSentence: 1 });
       return;
     }
 
-    if ([...'asdfg'].indexOf(key) > -1) {
+    if ([..."asdfg"].indexOf(key) > -1) {
       this.setState({ selectedSentence: 2 });
       return;
     }
 
-    if ([...'hjklnm'].indexOf(key) > -1) {
+    if ([..."hjklnm"].indexOf(key) > -1) {
       this.setState({ selectedSentence: 3 });
       return;
     }
 
-    if ([...'zxcvb'].indexOf(key) > -1) {
+    if ([..."zxcvb"].indexOf(key) > -1) {
       this.setState({ selectedSentence: 4 });
       return;
     }
 
-    if (key !== ' ' && key !== 'Backspace' && key !== 'Delete') {
+    if (key !== " " && key !== "Backspace" && key !== "Delete") {
       return;
     }
 
     let newValue;
-    if (this.state.letter === '\u25C0') {
+    if (this.state.letter === "\u25C0") {
       newValue = this.state[name].substring(1);
     } else {
       newValue = this.state.letter + this.state[name];
     }
 
     this.setState({ [name]: newValue }, () =>
-      this[`${name}Ref`].setSelectionRange(0, 0),
+      this[`${name}Ref`].setSelectionRange(0, 0)
     );
   };
 
@@ -126,6 +127,7 @@ class App extends Component {
             />
             <button onClick="">Log in</button>
           </form>
+          <Tip />
         </div>
       </div>
     );
---
+++ src/Tip.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./Tip.css";
+import Letter from "./Letter";
+
+class Tip extends Component {
+  state = {
+    open: false
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div className="tip-wrap">
+        <div
+          className="tip-icon close"
+          onClick={() => this.setState({ open: true })}
+        />
+        <div className={`tip-modal-wrap ${open ? "open" : ""}`}>
+          <div className="tip-modal">
+            <div className="tip-icon open" />
+            <div className="tips">
+              <div className="tip-header">Tips</div>
+              <div className="tip">
+                <span>Tip no. 1:</span>Use "Space" key to pick a letter
+              </div>
+              <div className="tip">
+                <span>Tip no. 2:</span>Use "Arrow Down/Up" key to change speed
+                of cursor
+              </div>
+              <div className="tip">
+                <span>Tip no. 3:</span>Use "Arrow Left/Right" key to change
+                cursor direction
+              </div>
+              <div className="tip">
+                <span>Tip no. 4:</span>Use "Letter" keys to change sentence
+              </div>
+              <div className="tip">
+                <span>Tip no. 5:</span>Inputs are filled from right to left
+              </div>
+            </div>
+            <div
+              className="close"
+              onClick={() => this.setState({ open: false })}
+            >
+              X
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tip;
